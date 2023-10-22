1894cab3fc92eb0221199eccb6c4ef173f4d8a2c
Florian Chrometz <florian@chrometz.me>
2023-10-22 12:11:25 +0200

    Tests now only reflect given function names

--- test/german-metadata-test.js
+++ test/german-metadata-test.js
@@ -3,40 +3,40 @@ const germanMetadata = require('../german-metadata');
 
 describe('German Metadata Package', function () {
   //getCityByPhoneNumber
-  it('should getCityByPhoneNumber', function () {
+  it('getCityByPhoneNumber', function () {
     expect(germanMetadata.getCityByPhoneNumber('+49 (0) 6971-492482').name).to.equal(
       'Frankfurt am Main',
     );
   });
 
   //getCityByPhonePrefix
-  it('should getCityByPhonePrefix', function () {
+  it('getCityByPhonePrefix', function () {
     expect(germanMetadata.getCityByPhonePrefix(69).name).to.equal('Frankfurt am Main');
   });
 
   //getPhonePrefixByCityName
-  it('should getPhonePrefixByCityName', function () {
+  it('getPhonePrefixByCityName', function () {
     expect(germanMetadata.getPhonePrefixByCityName('Frankfurt am Main')).to.equal(69);
   });
 
   //getPrefixOfPhoneNumber
-  it('should getPrefixOfPhoneNumber', function () {
+  it('getPrefixOfPhoneNumber', function () {
     expect(germanMetadata.getPrefixOfPhoneNumber('+49 (0) 6971-492482')).to.equal(69);
   });
 
   //doesCityExistWithName
-  it('should doesCityExistWithName', function () {
+  it('doesCityExistWithName', function () {
     expect(germanMetadata.doesCityExistWithName('Frankfurt am Main')).to.equal(true);
     expect(germanMetadata.doesCityExistWithName('Frankfurt am Rhein')).to.equal(false);
   });
 
   //normalizeCityName
-  it('should normalizeCityName', function () {
+  it('normalizeCityName', function () {
     expect(germanMetadata.normalizeCityName('Frankfurt am Main')).to.equal('frankfurt_am_main');
   });
 
   //normalizePhoneNumber
-  it('should normalizePhoneNumber', function () {
+  it('normalizePhoneNumber', function () {
     expect(germanMetadata.normalizePhoneNumber('+49 (0) 4171-492482')).to.equal('4171492482');
   });
 });
