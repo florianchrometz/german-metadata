b13bddc4f7f06d3fcf805dcd681315c3a0df7f7e
Florian Chrometz <florian@chrometz.me>
2023-10-22 12:00:15 +0200

    Updated testcases

--- test/german-metadata-test.js
+++ test/german-metadata-test.js
@@ -2,31 +2,41 @@ const { expect } = require('chai');
 const germanMetadata = require('../german-metadata');
 
 describe('German Metadata Package', function () {
-  it('should normalize city names correctly', function () {
+  //getCityByPhoneNumber
+  it('should getCityByPhoneNumber', function () {
+    expect(germanMetadata.getCityByPhoneNumber('+49 (0) 6971-492482').name).to.equal(
+      'Frankfurt am Main',
+    );
+  });
+
+  //getCityByPhonePrefix
+  it('should getCityByPhonePrefix', function () {
+    expect(germanMetadata.getCityByPhonePrefix(69).name).to.equal('Frankfurt am Main');
+  });
+
+  //getPhonePrefixByCityName
+  it('should getPhonePrefixByCityName', function () {
+    expect(germanMetadata.getPhonePrefixByCityName('Frankfurt am Main')).to.equal(69);
+  });
+
+  //getPrefixOfPhoneNumber
+  it('should getPrefixOfPhoneNumber', function () {
+    expect(germanMetadata.getPrefixOfPhoneNumber('+49 (0) 6971-492482')).to.equal(69);
+  });
+
+  //doesCityExistWithName
+  it('should doesCityExistWithName', function () {
+    expect(germanMetadata.doesCityExistWithName('Frankfurt am Main')).to.equal(true);
+    expect(germanMetadata.doesCityExistWithName('Frankfurt am Rhein')).to.equal(false);
+  });
+
+  //normalizeCityName
+  it('should normalizeCityName', function () {
     expect(germanMetadata.normalizeCityName('Frankfurt am Main')).to.equal('frankfurt_am_main');
   });
 
-  it('should normalize phone numbers correctly', function () {
+  //normalizePhoneNumber
+  it('should normalizePhoneNumber', function () {
     expect(germanMetadata.normalizePhoneNumber('+49 (0) 4171-492482')).to.equal('4171492482');
-  });
-
-  it('should find a city by phone number', function () {
-    const city = germanMetadata.getCityByPhoneNumber('+49 (0) 6971-492482');
-    expect(city.name).to.equal('Frankfurt am Main');
-  });
-
-  it('should find a city by phone prefix', function () {
-    const city = germanMetadata.getCityByPhonePrefix(69);
-    expect(city.name).to.equal('Frankfurt am Main');
-  });
-
-  it('should find a phone prefix by city name', function () {
-    const prefix = germanMetadata.getPhonePrefixByCityName('Frankfurt am Main');
-    expect(prefix).to.equal(69);
-  });
-
-  it('should find the phone prefix of a phone number', function () {
-    const prefix = germanMetadata.getPrefixOfPhoneNumber('+49 (0) 6971-492482');
-    expect(prefix).to.equal(69);
   });
 });
